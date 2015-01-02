a5c3f99184d201416f9b7275c7a16767b3dcc2c6
novelaspalo <novela.spalo@gmail.com>
2015-01-02 18:50:36 +0700

    template sudah jadi

--- beribukuweb/web/views.py
+++ beribukuweb/web/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render, request, redirect
+from django.shortcuts import render, redirect
+from urllib.request import Request
 
 # Create your views here.
 
-def program(request)
+def program(request):
+    data = {}
+    return render(request, '', data)
 
+def createProgram(request):
+    data = {}
+    return render(request, '', data)
+
+def editProgram(request):
     data = {}
     return render(request, '', data)
